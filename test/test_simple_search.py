27552313240496adcaca4148ff3b6f58ced95865
Florian Ludwig <f.ludwig@greyrook.com>
2014-04-20 21:01:57 +0200

    remove unit test for old search

--- test/test_simple_search.py
+++ test/test_simple_search.py
@@ -8,15 +8,16 @@ BASE_PATH = os.path.dirname(__file__) + '/examples'
 INDEX = None
 
 
-def setup_module(module):
-    global INDEX
-    INDEX = codeowl.Index(tempfile.mkdtemp())
-    INDEX.index(BASE_PATH + '/world.py')
-
-
-def teardown_module(module):
-    shutil.rmtree(INDEX.path)
-
-
-def test_search_string():
-    assert len(INDEX.search('hello_world')) == 2
+## index based search
+# def setup_module(module):
+#     global INDEX
+#     INDEX = codeowl.Index(tempfile.mkdtemp())
+#     INDEX.index(BASE_PATH + '/world.py')
+#
+#
+# def teardown_module(module):
+#     shutil.rmtree(INDEX.path)
+#
+#
+# def test_search_string():
+#     assert len(INDEX.search('hello_world')) == 2
